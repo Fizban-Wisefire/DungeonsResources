54b3b933afbf1da00f60bac17e1f3d7993992fc4
Patrick <patrickallenhoward@gmail.com>
2024-09-14 09:11:12 -0400

    Added and implemented the API functionality for /races.

---
+++ WebAPI/Controllers/RacesController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using Entities;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RacesController : ControllerBase
+    {
+
+        private readonly HttpClient _httpClient;
+
+        public RacesController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        // GET: api/<RacesController>
+        [HttpGet]
+        public async Task<List<Race>> Get()
+        {
+            List<Race> raceList = new List<Race>();
+            string fileName = "sampleData/races.json";
+            string filePath = Path.Combine(AppContext.BaseDirectory, "data", "races.json");
+            string jsonData = System.IO.File.ReadAllText(filePath);
+
+            raceList = JsonSerializer.Deserialize<List<Race>>(jsonData);
+
+            return raceList;
+        }
+
+        // GET api/<RacesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+    }
+}
